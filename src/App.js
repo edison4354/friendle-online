8e0e2569871f96421a18ebe9a23f9252af7c46eb
Edison Li <edison4354@gmail.com>
2022-03-14 20:28:52 -0700

    Keyboard Componenet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,200 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const row1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
+  const row2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
+  const row3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BACK'];
+
+  const [guess1, setGuess1] = useState('')
+  const [word, setWord] = useState('');
+
+  useEffect(() => {
+    function handleKeyDown(e) {
+      if (/^(?:[A-Za-z]+)$/.test(e.key) && e.key.length === 1) {
+        setWord(word + e.key.toUpperCase());
+      } else if (e.code === 'Backspace' || e.key === 'BACK') {
+        setWord(word.slice(0, -1));
+      } else {
+        return;
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return function cleanup() {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+
+  function handleKeyboardButton(button) {
+    console.log(`Button ${button}`);
+    if (button === 'BACK') {
+      setWord(word.slice(0, -1));
+    } else {
+      setWord(word + button);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="header">Friendle</p>
       </header>
+      <container className="App-container">
+        <div className="game-board">
+          <div className="game-row">
+            <div className="game-inner-row">
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+            </div>
+          </div>
+          <div className="game-row">
+            <div className="game-inner-row">
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+            </div>
+          </div>
+          <div className="game-row">
+            <div className="game-inner-row">
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+            </div>
+          </div>
+          <div className="game-row">
+            <div className="game-inner-row">
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+            </div>
+          </div>
+          <div className="game-row">
+            <div className="game-inner-row">
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+            </div>
+          </div>
+          <div className="game-row">
+            <div className="game-inner-row">
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+              <div className="game-tile">
+                <div className="tile"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="keyboard-row">
+          {row1.map((letter) => {
+            return (
+              <button
+                className="keyboard-button"
+                onClick={() => handleKeyboardButton(letter)}
+              >
+                {letter}
+              </button>
+            );
+          })}
+        </div>
+        <div className="keyboard-row">
+          {row2.map((letter) => {
+            return (
+              <button
+                className="keyboard-button"
+                onClick={() => handleKeyboardButton(letter)}
+              >
+                {letter}
+              </button>
+            );
+          })}
+        </div>
+        <div className="keyboard-row">
+          {row3.map((letter) => {
+            return (
+              <button
+                className="keyboard-button"
+                onClick={() => handleKeyboardButton(letter)}
+              >
+                {letter}
+              </button>
+            );
+          })}
+        </div>
+      </container>
     </div>
   );
 }
